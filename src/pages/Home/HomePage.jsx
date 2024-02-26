1f0f4322e13f4ec9ddafb2db64429ecfcba40e6d
Tanvi Jain <tanvijain1511@gmail.com>
2024-02-26 23:03:16 +0530

    Header with navbar & job

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,16 @@
 import { Button } from "@mui/material";
 import { Alert, AlertTitle } from "@mui/material";
 import useLogout from "../../hooks/useLogout";
+import Header from "../../components/Header/Navbar"
+import Notes from "../../components/Header/Job"
 
 const HomePage = () => {
   const { handleLogout, isLoggingOut, error } = useLogout();
 
   return (
     <>
+      <Header/>
+      <Notes/>
       <Button
         variant="contained"
         color="primary"
